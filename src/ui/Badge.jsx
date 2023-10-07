a2133ffe7a0185ef014a0453b2d41ece202cafda
master0827troy <mastertroy0827@gmail.com>
2023-10-08 01:58:36 +0300

    deleted the badge component new class

--- src/ui/Badge.jsx
+++ src/ui/Badge.jsx
@@ -1,18 +1,17 @@
-import { FaStar } from 'react-icons/fa6';
-import './Badge.css';
+import { FaStar } from "react-icons/fa6";
+import "./Badge.css";
 
 const Badge = (props) => {
-  let badgeClasses = 'badge';
+  let badgeClasses = "badge";
 
-  badgeClasses += props.type === 'top' ? ' top' : '';
-  badgeClasses += props.type === 'new' ? ' new' : '';
+  badgeClasses += props.type === "top" ? " top" : "";
 
   return (
     <div className={badgeClasses}>
-      <FaStar className='badge-icon' />
-      <span className='badge-text'>Top Rated</span>
+      <FaStar className="badge-icon" />
+      <span className="badge-text">Top Rated</span>
     </div>
-  )
+  );
 };
 
 export default Badge;
