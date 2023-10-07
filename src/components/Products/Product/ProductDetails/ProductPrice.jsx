7eb19e5cc97a95fdf3efca13bd2c2acd07e95e80
master0827troy <mastertroy0827@gmail.com>
2023-10-07 20:37:42 +0300

    updated the product price component

--- src/components/Products/Product/ProductDetails/ProductPrice.jsx
+++ src/components/Products/Product/ProductDetails/ProductPrice.jsx
@@ -1,16 +1,21 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const ProductPrice = ({vertical, price, discount}) => {
-  const newPrice = discount ? (price - ( price * discount / 100 )).toFixed(2) : price;
-  const containerClasses = vertical ? 'flex flex-row items-center gap-2' : 'space-y-1 text-center';
+const ProductPrice = ({ vertical, price, discount }) => {
+  const floatPrice = price.toFixed(2);
+  const newPrice = discount
+    ? (floatPrice - (floatPrice * discount) / 100).toFixed(2)
+    : floatPrice;
+
+  const containerClasses = vertical
+    ? "flex flex-row items-center gap-2"
+    : "space-y-1 text-center";
 
   return (
     <div className={containerClasses}>
-      <p className='text-lg font-bold'>${newPrice}</p>
-      {
-        newPrice !== price &&
-        <p className='text-base line-through'>${price}</p>
-      }
+      <p className="text-lg font-bold">${newPrice}</p>
+      {newPrice !== floatPrice && (
+        <p className="text-base line-through">${floatPrice}</p>
+      )}
     </div>
   );
 };
@@ -20,6 +25,5 @@ ProductPrice.propTypes = {
   price: PropTypes.number.isRequired,
   discount: PropTypes.number,
 };
-
 
 export default ProductPrice;
