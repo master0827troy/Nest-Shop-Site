4349e826b6aa588020b3d143d21ddd43adbf410d
master0827troy <mastertroy0827@gmail.com>
2023-10-08 17:07:58 +0300

    fixed ui issues in the product image

--- src/components/Products/Product/ProductDetails/ProductImage.jsx
+++ src/components/Products/Product/ProductDetails/ProductImage.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 const ProductImage = ({ id, title, image, className }) => {
-  const imageClasses = className ? "rounded-lg " + className : "rounded-lg ";
+  const imageClasses = className ? "w-full rounded-lg " + className : "w-full rounded-lg ";
 
   return (
     <>
       {id ? (
-        <Link to={`/product/${id}`} className="block p-3 bg-gray-100">
+        <Link to={`/product/${id}`} className="h-fit block p-3 bg-gray-100">
           <img src={image} alt={title} className={imageClasses} />
         </Link>
       ) : (
