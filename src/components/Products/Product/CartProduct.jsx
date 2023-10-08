f4b515e568bb4fdbc0e3f05c573bfa7969d4ff38
master0827troy <mastertroy0827@gmail.com>
2023-10-08 18:59:16 +0300

    updated cart products

--- src/components/Products/Product/CartProduct.jsx
+++ src/components/Products/Product/CartProduct.jsx
@@ -1,33 +1,36 @@
-import PropTypes from 'prop-types';
-import ProductImage from './ProductDetails/ProductImage';
-import ProductBadge from './ProductDetails/ProductBadge';
-import ProductTitle from './ProductDetails/ProductTitle';
-import ProductRating from './ProductDetails/ProductRating';
-import ProductStock from './ProductDetails/ProductStock';
-import ProductPrice from './ProductDetails/ProductPrice';
-import ProductDecreaseIcon from './ProductIcons/ProductDecreaseIcon';
-import ProductQuantity from './ProductDetails/ProductQuantity';
-import ProductIncreaseIcon from './ProductIcons/ProductIncreaseIcon';
-import ProductDeleteIcon from './ProductIcons/ProductDeleteIcon';
+import PropTypes from "prop-types";
+import ProductImage from "./ProductDetails/ProductImage";
+import ProductBadge from "./ProductDetails/ProductBadge";
+import ProductTitle from "./ProductDetails/ProductTitle";
+import ProductRating from "./ProductDetails/ProductRating";
+import ProductStock from "./ProductDetails/ProductStock";
+import ProductPrice from "./ProductDetails/ProductPrice";
+import ProductDecreaseIcon from "./ProductIcons/ProductDecreaseIcon";
+import ProductQuantity from "./ProductDetails/ProductQuantity";
+import ProductIncreaseIcon from "./ProductIcons/ProductIncreaseIcon";
+import ProductDeleteIcon from "./ProductIcons/ProductDeleteIcon";
 
 const CartProduct = ({ product }) => {
   return (
-    <div className='grid grid-cols-3 md:grid-cols-5 lg:grid-cols-6 gap-5 items-center md:items-start justify-between mb-10'>
-      <div className='grid col-span-3 md:col-span-2'>
-        <ProductImage id={product.id} title={product.title} image={product.image} className='w-full md:w-36 h-36' />
+    <div className="mb-10 flex flex-col lg:grid lg:grid-cols-6 gap-3">
+      <div className="grid col-span-3 md:col-span-2">
+        <ProductImage
+          id={product.id}
+          title={product.title}
+          image={product.image}
+          className="w-full h-36"
+        />
       </div>
-      <div className='col-span-3'>
-        <div className='flex flex-col items-center md:items-start'>
+      <div className="col-span-3 md:col-span-4 flex flex-col lg:grid lg:grid-cols-3">
+        <div className="col-span-2 flex flex-col items-start gap-1">
           <ProductBadge rating={product.rating} />
           <ProductTitle id={product.id} title={product.title} />
-          <ProductRating className='mb-2' max={5} rating={product.rating} />
+          <ProductRating className="mb-1" max={5} rating={product.rating} />
           <ProductStock stock={product.stock} />
         </div>
-      </div>
-      <div className='col-span-3 lg:col-span-1 text-center'>
-        <div className='flex flex-row lg:flex-col justify-center md:justify-start items-center gap-2'>
+        <div className="col-span-1 flex flex-row lg:flex-col justify-start items-center gap-2">
           <ProductPrice price={product.price} discount={product.discount} />
-          <div className='mt-2 flex flex-row items-center gap-2 mb-3'>
+          <div className="mt-2 flex flex-row items-center gap-2 mb-3">
             <ProductDecreaseIcon id={product.id} />
             <ProductQuantity quantity={product.quantity} />
             <ProductIncreaseIcon id={product.id} />
@@ -42,6 +45,5 @@ const CartProduct = ({ product }) => {
 CartProduct.propTypes = {
   product: PropTypes.object.isRequired,
 };
-
 
 export default CartProduct;
