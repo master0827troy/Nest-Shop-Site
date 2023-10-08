bc6b3f4ba0565662557387c67260506e9072fd9d
master0827troy <mastertroy0827@gmail.com>
2023-10-09 02:47:09 +0300

    added optional prop className to the ProductTitle component

--- src/components/Products/Product/ProductDetails/ProductTitle.jsx
+++ src/components/Products/Product/ProductDetails/ProductTitle.jsx
@@ -1,17 +1,25 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const ProductTitle = ({ id, title }) => {
+const ProductTitle = ({ id, title, className }) => {
   return (
     <>
       {id ? (
         <Link to={`/product/${id}`}>
-          <p className="max-w-xs text-base text-gray-700 font-semibold leading-5 tracking-tight">
+          <p
+            className={`max-w-xs text-base text-gray-700 font-semibold leading-5 tracking-tight ${
+              className || ""
+            }`}
+          >
             {title.length > 30 ? title.substring(0, 62) + "..." : title}
           </p>
         </Link>
       ) : (
-        <p className="text-base text-gray-700 font-semibold leading-5 tracking-tight">
+        <p
+          className={`text-base text-gray-700 font-semibold leading-5 tracking-tight ${
+            className || ""
+          }`}
+        >
           {title}
         </p>
       )}
@@ -22,6 +30,7 @@ const ProductTitle = ({ id, title }) => {
 ProductTitle.propTypes = {
   id: PropTypes.string,
   title: PropTypes.string.isRequired,
+  className: PropTypes.string,
 };
 
 export default ProductTitle;
