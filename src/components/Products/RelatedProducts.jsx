9fe71f53d2ac2f430413bc3d419bf73bddc6a312
master0827troy <mastertroy0827@gmail.com>
2023-10-09 01:26:27 +0300

    related products => popular products

--- src/components/Products/RelatedProducts.jsx
+++ src/components/Products/RelatedProducts.jsx
@@ -5,7 +5,7 @@ import Heading from '../Heading';
 const RelatedProducts = ({ products }) => {
   return (
     <div className='flex flex-col'>
-      <Heading heading='Similar Products' className='mb-8' />
+      <Heading heading='Popular Products' className='mb-8' />
       <div className='w-full xl:w-64 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:flex xl:flex-col gap-8'>
         {
           products.map(product =>
