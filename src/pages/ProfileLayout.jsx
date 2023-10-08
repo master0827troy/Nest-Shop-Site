698dfd26d334a1d789f891aaae38a21ae2933ec0
master0827troy <mastertroy0827@gmail.com>
2023-10-08 20:44:17 +0300

    sticky profile nav

--- src/pages/ProfileLayout.jsx
+++ src/pages/ProfileLayout.jsx
@@ -16,31 +16,36 @@ const ProfileLayout = () => {
 
   return (
     <>
-      <div className='mt-12 mb-6 ml-4 flex flex-row gap-2'>
-        <AiOutlineUser className='text-3xl' />
-        <h3 className='text-2xl font-semibold'>Hello, {auth.currentUser.displayName}</h3>
+      <div className="sticky top-8 mt-12 mb-6 ml-4 flex flex-row gap-2">
+        <AiOutlineUser className="text-3xl" />
+        <h3 className="text-2xl font-semibold">
+          Hello, {auth.currentUser.displayName}
+        </h3>
       </div>
-      <div className='flex flex-col lg:flex-row gap-8'>
-        <div>
-          <ul className='md:w-max grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-none text-xl font-semibold'>
-            {
-              list.map(listItem =>
-                <NavLink key={listItem.id} to={listItem.path} 
-                  className={({ isActive }) => isActive ? 'text-orange-600 transition duration-500' : 'hover:text-orange-600 transition duration-500'}>
-                  <li className='py-2 px-4 cursor-pointer'>
-                    {listItem.text}
-                  </li>
-                </NavLink>
-              )
-            }
+      <div className="flex flex-col lg:flex-row gap-8">
+        <div className="sticky top-20 h-fit">
+          <ul className="md:w-max grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-none text-xl font-semibold">
+            {list.map((listItem) => (
+              <NavLink
+                key={listItem.id}
+                to={listItem.path}
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-orange-600 transition duration-500"
+                    : "hover:text-orange-600 transition duration-500"
+                }
+              >
+                <li className="py-2 px-4 cursor-pointer">{listItem.text}</li>
+              </NavLink>
+            ))}
           </ul>
         </div>
-        <div className='grow'>
+        <div className="grow">
           <Outlet />
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default ProfileLayout
