a4e9a890f6a3018b1760a43e028d5b0c9a1c65b9
master0827troy <mastertroy0827@gmail.com>
2023-10-09 05:32:32 +0300

    prevented search input form submit

--- src/ui/SearchBar.jsx
+++ src/ui/SearchBar.jsx
@@ -4,37 +4,42 @@ import { FaSearch } from "react-icons/fa";
 const SearchBar = (props) => {
   const [isActive, setIsActive] = useState(null);
   const [placeholder, setPlaceholder] = useState(props.placeholder);
-  const [counter, setCounter] = useState(0)
-  const [operation, setOperation] = useState(null)
+  const [counter, setCounter] = useState(0);
+  const [operation, setOperation] = useState(null);
 
   const editPlaceholder = (value) => {
-    setCounter(prevCounter => prevCounter + value);
-    if(operation === 'delete') {
-      setPlaceholder(prevPlaceholder => 
-        prevPlaceholder.substr(0, (prevPlaceholder.length - value))
+    setCounter((prevCounter) => prevCounter + value);
+    if (operation === "delete") {
+      setPlaceholder((prevPlaceholder) =>
+        prevPlaceholder.substr(0, prevPlaceholder.length - value)
       );
-    } else if(operation === 'write') {
-      setPlaceholder(prevPlaceholder => 
-        prevPlaceholder + props.placeholder.substr(prevPlaceholder.length, 1)
+    } else if (operation === "write") {
+      setPlaceholder(
+        (prevPlaceholder) =>
+          prevPlaceholder + props.placeholder.substr(prevPlaceholder.length, 1)
       );
     }
-  }
+  };
 
   const focusHandler = () => {
-    setIsActive(true)
-    setOperation('delete')
+    setIsActive(true);
+    setOperation("delete");
   };
 
   const blurHandler = () => {
-    setIsActive(false)
-    setOperation('write')
+    setIsActive(false);
+    setOperation("write");
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
   };
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      if (operation === 'delete' && counter < props.placeholder.length) {
+      if (operation === "delete" && counter < props.placeholder.length) {
         editPlaceholder(1);
-      } else if(operation === 'write' && counter > 0) {
+      } else if (operation === "write" && counter > 0) {
         editPlaceholder(-1);
       }
     }, 60);
@@ -44,16 +49,18 @@ const SearchBar = (props) => {
     };
   }, [counter, editPlaceholder, operation, props.placeholder.length]);
 
-  let containerClasses = 'search-bar-container';
-  containerClasses += isActive ? ' active' : '';
-  containerClasses += props.containerClass ? ' ' + props.containerClass : '';
+  let containerClasses = "search-bar-container";
+  containerClasses += isActive ? " active" : "";
+  containerClasses += props.containerClass ? " " + props.containerClass : "";
 
-  const inputClasses = props.inputClass ? 'search-bar' + ' ' + props.inputClass : 'search-bar';
+  const inputClasses = props.inputClass
+    ? "search-bar" + " " + props.inputClass
+    : "search-bar";
 
   return (
-    <form className={containerClasses}>
-      <input 
-        type="text" 
+    <form className={containerClasses} onSubmit={submitHandler}>
+      <input
+        type="text"
         id="search"
         placeholder={placeholder}
         className={inputClasses}
@@ -62,8 +69,11 @@ const SearchBar = (props) => {
         onFocus={focusHandler}
         onBlur={blurHandler}
       />
-      <div className='icon-container' onClick={props.onIconClick ? props.onIconClick : () => {}}>
-        <FaSearch className='icon'/>
+      <div
+        className="icon-container"
+        onClick={props.onIconClick ? props.onIconClick : () => {}}
+      >
+        <FaSearch className="icon" />
       </div>
     </form>
   );
