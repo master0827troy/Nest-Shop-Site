37d356354e0e1de546ab03a1e81ce14d66ebe045
master0827troy <mastertroy0827@gmail.com>
2023-10-09 13:13:27 +0300

    fixed a navigation error

--- src/components/Promotions.jsx
+++ src/components/Promotions.jsx
@@ -1,22 +1,36 @@
-import Button from '../ui/Button';
-import './Promotions.css';
+import { useNavigate } from "react-router-dom";
+import Button from "../ui/Button";
+import "./Promotions.css";
 
-const promotions = (props) => {
+const Promotions = (props) => {
+  const navigate = useNavigate();
+
   return (
-    <div className='promotions'>
-      {
-        props.promotions.map(promotion =>
-          <div key={promotion.id} className={promotion.imageClasses ? 'promotion ' + promotion.imageClasses : 'promotion'} style={{backgroundImage: `url('${promotion.image}')`}}>
-            <div className='promotion-content'>
-              <h3 className='promotion-title'>{promotion.title}</h3>
-              <p className='promotion-text'>{promotion.text}</p>
-              <Button text='Shop now' className='text-white !border-orange-600 rounded-sm' noBg onClick={() => navigate(promotion.path)} />
-            </div>
+    <div className="promotions">
+      {props.promotions.map((promotion) => (
+        <div
+          key={promotion.id}
+          className={
+            promotion.imageClasses
+              ? "promotion " + promotion.imageClasses
+              : "promotion"
+          }
+          style={{ backgroundImage: `url('${promotion.image}')` }}
+        >
+          <div className="promotion-content">
+            <h3 className="promotion-title">{promotion.title}</h3>
+            <p className="promotion-text">{promotion.text}</p>
+            <Button
+              text="Shop now"
+              className="text-white !border-orange-600 rounded-sm"
+              noBg
+              onClick={() => navigate(promotion.path)}
+            />
           </div>
-        )
-      }
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default promotions
+export default Promotions;
