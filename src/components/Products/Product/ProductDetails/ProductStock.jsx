db974eae2d41ee0bc6a80dcb68e9823f8b99232a
master0827troy <mastertroy0827@gmail.com>
2023-10-14 00:56:17 +0300

    changed stock text

--- src/components/Products/Product/ProductDetails/ProductStock.jsx
+++ src/components/Products/Product/ProductDetails/ProductStock.jsx
@@ -4,7 +4,7 @@ const ProductStock = ({ stock }) => {
   return (
     <div className="group flex flex-row items-center">
       <span className="inline-block w-2 h-2 bg-slate-900 rounded-full mr-2 group-hover:animate-ping"></span>
-      <span>{stock} units left in stock</span>
+      <span>{stock} items left</span>
     </div>
   );
 };
